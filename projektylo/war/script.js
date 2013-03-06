68c1f33c9de5b134ce0a3ad20806a8c38357d291
rainerv <rainerv@ut.ee>
2013-03-06 17:58:44 +0200

    Tegin väheke statistika asju mingi algelise menüü aga seal see body ei tööta hetkel kui content väiksem siis nupud ei ole content peal vms peab tuunima vähe

--- projektylo/war/script.js
+++ projektylo/war/script.js
@@ -11,6 +11,42 @@ $(document).ready(function()
 		$('div.alltabs ul.tabs li a').removeClass('tab-current');
 		$(this).addClass('tab-current');
 	});
+	
+	$('div.statPiirkondContent').hide();
+	$('div.statErakondContent').hide();
+	$('div.statKandidaadidContent').hide();
+	$('div.statRiiklikContent').hide();
+	
+	$("#statPiirkond").click(function()
+		{
+		$('div.statPiirkondContent').show();
+		$('div.statErakondContent').hide();
+		$('div.statKandidaadidContent').hide();
+		$('div.statRiiklikContent').hide();
+		});
+	$("#statErakond").click(function()
+		{
+		$('div.statPiirkondContent').hide();
+		$('div.statErakondContent').show();
+		$('div.statKandidaadidContent').hide();
+		$('div.statRiiklikContent').hide();
+		});
+	$("#statKandidaadid").click(function()
+		{
+		$('div.statPiirkondContent').hide();
+		$('div.statErakondContent').hide();
+		$('div.statKandidaadidContent').show();
+		$('div.statRiiklikContent').hide();	
+		});
+	$("#statRiiklik").click(function()
+		{
+		$('div.statPiirkondContent').hide();
+		$('div.statErakondContent').hide();
+		$('div.statKandidaadidContent').hide();
+		$('div.statRiiklikContent').show();	
+		});
+	
+	
 	// siia piirkonna funktsioon
 
 		      $("#valiPiirkond").click(function (e)
