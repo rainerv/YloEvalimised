d180adba26c0c3c8ecf0f09d713d31ac82a4e0be
rainerv <rainerv@ut.ee>
2013-03-06 14:14:39 +0200

    Javascript commit, jquery menüü, minu andmed leht töötab piirkonna valimine

---
+++ projektylo/war/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function() 
+{
+	$('div.alltabs div.tabsContent').hide();
+	$('div.tab1').show();
+	$('div.alltabs ul.tabs li.tab1 a').addClass('tab-current');
+	$('div.alltabs ul li a').click(function()
+	{
+		var thisClass = this.className.slice(0,4);
+		$('div.alltabs div.tabsContent').hide();
+		$('div.' + thisClass).show();
+		$('div.alltabs ul.tabs li a').removeClass('tab-current');
+		$(this).addClass('tab-current');
+	});
+	// siia piirkonna funktsioon
+
+		      $("#valiPiirkond").click(function (e)
+		      {
+		         ShowDialog(true);
+		         e.preventDefault();
+		      });
+
+		      $("#sulgeButton").click(function (e)
+		      {
+		         HideDialog();
+		         e.preventDefault();
+		      });
+
+		      $("#valiButton").click(function (e)
+		      {
+		         var ringkond = $("#ringkond option:selected").text();
+		         $("#piirkond").html("Piirkond: "+ringkond);
+		         $("#test").html("Kandidatuur: Puudub!")
+		         HideDialog();
+		         e.preventDefault();
+		         $("#valiPiirkond").hide();
+		      });
+
+		   function ShowDialog(modal)
+		   {
+		      $("#overlay").show();
+		      $("#dialog").fadeIn(300);
+
+		      if (modal)
+		      {
+		         $("#overlay").unbind("click");
+		      }
+		      else
+		      {
+		         $("#overlay").click(function (e)
+		         {
+		            HideDialog();
+		         });
+		      }
+		   }
+
+		   function HideDialog()
+		   {
+		      $("#overlay").hide();
+		      $("#dialog").fadeOut(300);
+		   } 
+	
+	
+	//piirkonna funktsioon lõpp
+	
+});
